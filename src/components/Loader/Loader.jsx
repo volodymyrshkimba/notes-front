d88d0d45f1b93bec920737d0fa53c7f365adc744
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-06-04 22:56:05 +0300

    add message for long loading

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,27 @@
 import { ClipLoader } from "react-spinners";
-
 import css from "./Loader.module.css";
+import { useEffect, useState } from "react";
 
 const Loader = () => {
+  const [message, setMessage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMessage(true);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className={css.loaderWrapper}>
       <ClipLoader />
+      {message && (
+        <p className={css.message}>
+          If the loading takes longer than expected, please be patient — it’s
+          running on a free server and may need extra time to wake up.
+        </p>
+      )}
     </div>
   );
 };
